d9b6606182663c1859da3c13d8edc9d12fbf6ac2
Seok-Hun <hamer1233@naver.com>
2024-01-18 11:39:07 +0900

    Feat: 프로젝트 생성 이벤트 서비스 프로젝트 생성

---
+++ src/main/java/kea/dpang/eventserver/EventServerApplication.java
@@ -0,0 +1,13 @@
+package kea.dpang.eventserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/dpang/eventserver/EventServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.dpang.eventserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
