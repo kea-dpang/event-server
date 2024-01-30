bdcb510b8cc97a06ad4eb0c919cdeb5b615c86cb
Seok-Hun <hamer1233@naver.com>
2024-01-30 14:59:35 +0900

    Fix: 페인 클라이언트 name 수정 -service를 -server로 변경

--- src/main/java/kea/dpang/eventserver/client/SellerSerivceClient.java
+++ src/main/java/kea/dpang/eventserver/client/SellerSerivceClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "seller-service")
+@FeignClient(name = "seller-server")
 // eureka를 사용하므로 url은 생략
 public interface SellerSerivceClient {
     @GetMapping("/api/seller/findName")
